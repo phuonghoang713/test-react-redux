4134992f641360d1da7e03e624e7d6ed99039563
Hoang Nguyen <phuonghoang713@gmail.com>
2018-09-21 16:45:37 +0700

    add some file

---
+++ src/reducers/rootReducers.js
@@ -0,0 +1,9 @@
+  // reducers/rootReducers.js
+  import { combineReducers } from 'redux';
+  import todosReducers from './todosReducers';
+
+  const rootReducer = combineReducers({
+      todosReducers
+  });
+
+  export default rootReducer;
